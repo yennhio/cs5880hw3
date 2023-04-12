a2a02251db973d98fe9a852ad406de578bcf8a27
Yennhi Dang <nhidangn@gmail.com>
2023-04-11 21:05:27 -0700

    implement loyal statuses

--- decorator/LoyaltyStatus.java
+++ decorator/LoyaltyStatus.java
@@ -1,9 +1,26 @@
-//package decorator;
+package decorator;
 
-//public class LoyaltyStatus {
+public class LoyaltyStatus {
 
-//    public double applyDiscount() {
-        //return total;
-    //}
+    int status;
+
+    public LoyaltyStatus(int status) {
+        this.status = status;
+    }
+
+    public double applyDiscount(double cost) {
+
+        if(status == 1) {
+            cost *= 0.9;
+        }
+        else if (status == 2){
+            cost *= 0.8;
+        }
+        else if (status == 3) {
+            cost *= 0.7;
+        }
+
+        return cost;
+    }
     
-//}
+}
